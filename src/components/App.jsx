e7cdb078284a842c9828843301a20fb6d814b71a
Anna Stalevska-Bondarenko <anitastal.89@gmail.com>
2022-10-24 10:36:18 +0100

    add files from 2 rep

---
+++ src/components/App.jsx
@@ -0,0 +1,93 @@
+import { Component } from 'react';
+import { Phonebook } from './Phonebook';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+
+export class App extends Component {
+  static propTypes = {
+    filter: PropTypes.string,
+    contacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+      })
+    ),
+  };
+
+  state = {
+    contacts: [
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleDeleteUser = id => {
+    this.setState(prevState => {
+      return { contacts: prevState.contacts.filter(item => item.id !== id) };
+    });
+  };
+
+  onFilterContacts = () => {
+    return this.state.contacts.filter(item =>
+      item.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+  };
+
+  onChangeName = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (name, number) => {
+    if (this.state.contacts.some(contact => contact.name === name)) {
+      return alert(`${name} is already in contacts`);
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            name,
+            number,
+            id: nanoid(),
+          },
+        ],
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          fontSize: 16,
+          color: '#010101',
+          gap: 10,
+        }}
+      >
+        <h1>Phonebook</h1>
+        <Phonebook
+          handleAddContact={this.handleSubmit}
+          onChangeName={this.onChangeName}
+        />
+        <h2>Contacts</h2>
+        <Filter onChangeName={this.onChangeName} filter={this.state.filter} />
+        <ContactList
+          onFilterContacts={this.onFilterContacts()}
+          deleteUser={this.handleDeleteUser}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class ContactList extends Component {
+  render() {
+    const { onFilterContacts, deleteUser } = this.props;
+
+    return (
+      <ul>
+        {onFilterContacts.map(contact => (
+          <li key={contact.id}>
+            <p>{contact.name}</p>
+            <p>{contact.number}</p>
+            <button onClick={() => deleteUser(contact.id)} type="button">
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+ContactList.propTypes = {
+  onFilterContacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      number: PropTypes.string,
+      name: PropTypes.string,
+      key: PropTypes.string,
+      id: PropTypes.string,
+    })
+  ),
+  deleteUser: PropTypes.func,
+};
